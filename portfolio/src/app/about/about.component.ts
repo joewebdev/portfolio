7d83f8251c6b14ae22635591f70fa3417529c783
Joe Garite <webreality@gmail.com>
2019-05-31 21:02:31 -0400

    started adding about content

--- portfolio/src/app/about/about.component.ts
+++ portfolio/src/app/about/about.component.ts
@@ -7,6 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+    skills = [
+        {
+            name: 'HTML/CSS',
+            level: 100,
+            sort:0
+        },
+        {
+            name: 'Javascript ES5',
+            level: 90,
+            sort:1
+        },
+        {
+            name: 'Javascript ES6',
+            level: 40,
+            sort: 2
+        },
+        {
+            name: 'AngularJS',
+            level: 90,
+            sort: 3
+        },
+        {
+            name: 'Angular 7/8',
+            level: 15,
+            sort: 4
+        },
+        {
+            name: 'Performance Optimization',
+            level: 90,
+            sort: 5
+        },
+        {
+            name: 'Search Engine Optimization',
+            level: 90,
+            sort: 6
+        },
+        {
+            name: 'VueJS',
+            level: 10,
+            sort: 7
+        },
+        {
+            name: 'React',
+            level: 10,
+            sort: 8
+        }
+    ]
+    softSkills = [
+        {
+            name: 'Team management',
+            level: 90,
+            sort: 1
+        },
+        {
+            name: 'Technical Leadership',
+            level: 90,
+            sort: 1
+        },
+        {
+            name: 'HTML/CSS',
+            level: 100,
+            sort: 1
+        }
+    ]
   constructor() { }
 
   ngOnInit() {
